1c2fc727405965b7bd010c0c66d09e3b710a0cec
Victor Valencia <vicvr8_08@hotmail.com>
2022-07-17 08:41:26 -0500

    Primer Script

---
+++ hola_mundo.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+# Imprime un Hola mundo
+# Victor Valencia - @vicvr823
+
+echo "Hola mundo"
+exit 0
+# exit 0 - El script se ejecutó correctamente
+# exit 1 - errores generales, como por ejemplo, division por cero
+# exit 2 - errores shell
